7b67d3b7dd6557faefe2972e63779ef24cb5f82b
IgorKowalczyk <majonezexe@gmail.com>
2020-06-19 20:29:50 +0200

    Add font end delete index.js

--- popup/index.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.setBadgeBackgroundColor({
-	color: '#333333'
-});
-chrome.browserAction.setBadgeText({
-	text: 'Hi! Im a browser extension that prevents Google and Yandex search pages from modifying search result links! (test)'
-});
