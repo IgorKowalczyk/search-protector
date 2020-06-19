4e254de8d5d759511e17f9e697a83d3442db5336
IgorKowalczyk <majonezexe@gmail.com>
2020-06-19 12:04:23 +0200

    Update popup

---
+++ popup/index.js
@@ -0,0 +1,6 @@
+chrome.browserAction.setBadgeBackgroundColor({
+	color: '#333333'
+});
+chrome.browserAction.setBadgeText({
+	text: 'Hi! Im a browser extension that prevents Google and Yandex search pages from modifying search result links! (test)'
+});
