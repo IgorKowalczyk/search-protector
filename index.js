2476d0f43af1523eb25bdc484b5e63035e9d5de0
IgorKowalczyk <majonezexe@gmail.com>
2020-06-21 11:14:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-window.addEventListener("mousedown", linktarget, true);
-window.addEventListener("mousedown", restorelinktarget, false);
+window.addEventListener("mousedown", saveLinkTarget, true);
+window.addEventListener("mousedown", restoreLinkTarget, false);
 window.addEventListener("click", interceptEvent, true);
 window.addEventListener("keydown", interceptEvent, true);
 
@@ -7,19 +7,19 @@ function detach()
 {
   try
   {
-    window.removeEventListener("mousedown", linktarget, true);
-    window.removeEventListener("mousedown", restorelinktarget, false);
-    window.removeEventListener("click", interceptevent, true);
-    window.removeEventListener("keydown", interceptevent, true);
+    window.removeEventListener("mousedown", saveLinkTarget, true);
+    window.removeEventListener("mousedown", restoreLinkTarget, false);
+    window.removeEventListener("click", interceptEvent, true);
+    window.removeEventListener("keydown", interceptEvent, true);
   }
   catch (e)
   {
-    return;
+
   }
 }
 
-let currentlink = null;
-let currentlinkblock = null;
+let currentLink = null;
+let currentLinkHref = null;
 
 let hosts = {
   "groups.google.com": "google-groups",
@@ -32,13 +32,13 @@ let hosts = {
   "yandex.ua": "yandex",
 };
 
-let containerattr = {
+let containerAttr = {
   "google": "#search,.gsc-wrapper",
   "google-groups": "[role=main]",
   "google-images": "[data-cid^=GRID]",
 };
 
-function search(window)
+function isSearchPage(window)
 {
   try
   {
@@ -52,11 +52,11 @@ function search(window)
   }
   catch (e)
   {
-   return;
+
   }
 
   for (let type of ["google", "google-images"])
-    if (document instanceof HTMLDocument && document.querySelector(containerattr[type]))
+    if (document instanceof HTMLDocument && document.querySelector(containerAttr[type]))
       return type;
 
   if (document.readyState == "complete")
@@ -64,15 +64,15 @@ function search(window)
   return null;
 }
 
-function searchresoult(link)
+function isSearchResult(link)
 {
-  let type = search(link.ownerDocument.defaultView);
+  let type = isSearchPage(link.ownerDocument.defaultView);
   if (type === null)
     return false;
 
-  if (containerattr.hasOwnProperty(type))
+  if (containerAttr.hasOwnProperty(type))
   {
-    let selector = containerattr[type];
+    let selector = containerAttr[type];
     for (let parent = link; parent; parent = parent.parentNode)
       if ("matches" in parent && parent.matches(selector))
         return true;
@@ -80,39 +80,39 @@ function searchresoult(link)
   return false;
 }
 
-function savelinktarget(event)
+function saveLinkTarget(event)
 {
-  let type = search(event.target.ownerDocument.defaultView);
+  let type = isSearchPage(event.target.ownerDocument.defaultView);
   if (!type)
     return;
 
-  for (currentlink = event.target; currentlink; currentlink = currentlink.parentNode)
-    if (currentlink.localName == "a")
+  for (currentLink = event.target; currentLink; currentLink = currentLink.parentNode)
+    if (currentLink.localName == "a")
       break;
 
-  currentlinkhref = (currentlink ? currentlink.href : null);
+  currentLinkHref = (currentLink ? currentLink.href : null);
 
-  if (type == "yandex" && currentlink)
-    currentlink.removeAttribute("data-counter");
-  setTimeout(restorelinktarget, 0);
+  if (type == "yandex" && currentLink)
+    currentLink.removeAttribute("data-counter");
+  setTimeout(restoreLinkTarget, 0);
 }
 
-function restorelintarget(event)
+function restoreLinkTarget(event)
 {
   try
   {
-    if (currentlink && currentlink.href != currentlinkhref)
-      currentlink.href = currentlinkhref;
+    if (currentLink && currentLink.href != currentLinkHref)
+      currentLink.href = currentLinkHref;
   }
   catch (e)
   {
-    return;
+	  
   }
 
-  currentlink = currentlinkhref = null;
+  currentLink = currentLinkHref = null;
 }
 
-function interceptevent(event)
+function interceptEvent(event)
 {
   if (event.type == "keydown" && event.keyCode != event.DOM_VK_RETURN)
     return;
